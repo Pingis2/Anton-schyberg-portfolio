481b222c9f2d661796ec0c0f690edac6f25b05ef
Pingis2 <anton@worksit.se>
2025-07-08 15:51:36 +0200

    updated footer and header

--- portfolio/src/components/Footer.tsx
+++ portfolio/src/components/Footer.tsx
@@ -1,8 +1,8 @@
 export function Footer() {
     return (
-        <footer>
+        <>
             <p>© 2023 Anton Schyberg</p>
             <p>Contact:</p>
-        </footer>
+        </>
     )
 }
--- portfolio/src/components/Header.tsx
+++ portfolio/src/components/Header.tsx
@@ -1,18 +1,35 @@
-import { useNavigate } from "react-router-dom";
+import { useNavigate, useLocation } from "react-router-dom";
 
 
 function Header() {
 
     const navigate = useNavigate();
+    const location = useLocation();
+    
     return (
+        <>
+        <p onClick={() => navigate("/")}>Anton Schyberg</p>
         <ul>
             <li>
-                <button type="submit" onClick={() => navigate("/")}>Home</button>
+                <button 
+                    type="submit" 
+                    onClick={() => navigate("/")}
+                    className={location.pathname === "/" ? "active" : ""}
+                >
+                    Home
+                </button>
             </li>
             <li>
-                <button type="submit" onClick={() => navigate("/about")}>About</button>
+                <button 
+                    type="submit" 
+                    onClick={() => navigate("/about")}
+                    className={location.pathname === "/about" ? "active" : ""}
+                >
+                    About
+                </button>
             </li>
         </ul>
+        </>
     )
 }
 
